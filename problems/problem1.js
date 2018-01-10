dcb50262e6520d1458278114553050beac982aab
Kamylle <kamylle.b.g@gmail.com>
2018-01-10 15:56:29 -0500

    Problem 1 Done

--- problems/problem1.js
+++ problems/problem1.js
@@ -2,16 +2,26 @@ var assert = require('assert');
 
 // we need 5 test cases. I provided 1 input
 let inputs = [
-  ""
+  "",
+  "abc",
+  "12345",
+  "hello",
+  "This is a string"
 ]
 
 let outputs = [
-  
+  undefined,
+  "a",
+  "1",
+  "h",
+  "T"
 ]
 
 // Make this function return the first letter of the string that is passed to it. If the string does not have a first letter, return undefined
 function f(str) {
-    
+  if (str.length > 0){
+    return str.charAt(0);
+  };
 }
 
 function runTest(i) {
