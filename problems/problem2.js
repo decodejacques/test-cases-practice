dda2592abab764af1170fa589ac38b0b49bb0c16
Archer143 <Alex.F.April@hotmail.com>
2018-01-10 15:41:31 -0500

    The second problem has been solved

--- problems/problem2.js
+++ problems/problem2.js
@@ -2,16 +2,27 @@ var assert = require('assert');
 
 // we need 5 test cases.
 let inputs = [
-
+    "",
+    "abc",
+    "cba",
+    "123",
+    "321"
 ]
 
 let outputs = [
-  
+    undefined,
+    "c",
+    "a",
+    "3",
+    "1"
 ]
 
 // Make this function return the last letter of the string that is passed to it. If the string does not have a last letter, return undefined
 function f(str) {
-    
+    var array = str.split("");
+    var reversed = array.reverse();
+    var firstLetter = reversed[0];
+    return firstLetter;
 }
 
 function runTest(i) {
