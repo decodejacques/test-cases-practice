086e452c1d967262fb550fdee5c1becccfcd58da
Kamylle <kamylle.b.g@gmail.com>
2018-01-10 16:23:49 -0500

    Problem 2 Done

--- problems/problem2.js
+++ problems/problem2.js
@@ -2,16 +2,26 @@ var assert = require('assert');
 
 // we need 5 test cases.
 let inputs = [
-
+  "",
+  "abc",
+  "12345",
+  "hello",
+  "This is a string"
 ]
-
-let outputs = [
   
+let outputs = [
+  undefined,
+  "c",
+  "5",
+  "o",
+  "g"
 ]
 
 // Make this function return the last letter of the string that is passed to it. If the string does not have a last letter, return undefined
 function f(str) {
-    
+    if (str.length > 0) {
+        return str.charAt(str.length - 1);
+    }
 }
 
 function runTest(i) {
