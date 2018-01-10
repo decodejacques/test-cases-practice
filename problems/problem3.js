80fb83c5123586cf1dfcdbf5667c4aa9fadc4d3e
Kamylle <kamylle.b.g@gmail.com>
2018-01-10 17:20:36 -0500

    Problem 3 Done

--- problems/problem3.js
+++ problems/problem3.js
@@ -3,19 +3,32 @@ var assert = require('assert');
 // we need 7 test cases. I've provided 2.
 let inputs = [
   [2, 4],
-  [-3, 3]
+  [-3, 3],
+  ["a", 8],
+  ["x", "z"],
+  [21, 21],
+  [3, 39],
+  [8, 5]
 ]
 
 let outputs = [
   6,
-  0
+  0,
+  undefined,
+  undefined,
+  42,
+  42,
+  13
 ]
 
 /*
 Make this function return the sum of the two numbers that are passed to it. If one of the numbers is not passed, or if anything other than numbers are passed, return undefined.
 */
-function f(x, y) {
-    
+function f(x) {
+  console.log(x)
+  if (!isNaN(x[0]) & !isNaN(x[1])) {
+    return x[0] + x[1];
+  }
 }
 
 function runTest(i) {
