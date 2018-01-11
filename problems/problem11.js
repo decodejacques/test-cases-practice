4f6f0a2fe96c4cf35de3c0a7751abccb20fa9ffd
Kamylle <kamylle.b.g@gmail.com>
2018-01-10 22:10:48 -0500

    Problem 11 Done

--- problems/problem11.js
+++ problems/problem11.js
@@ -2,18 +2,33 @@ var assert = require('assert');
 
 // we need 5 test cases. 
 let inputs = [
-  
+  [1, 2, 3, 4, 5],
+  [40, 2],
+  ["a", 13],
+  ["x", "y"],
+  []
 ]
 
 let outputs = [
-  
+  15,
+  42,
+  13,
+  0,
+  0
 ]
 
 /*
 Make this function return the sum of all the numbers in the input array. If any element in the array is not a number, skip it. If the array is empty, return zero.
 */
 function f(arr) {
-    
+    var answer = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (typeof arr[i] == 'number') {
+            answer = answer + arr[i];
+        }
+    }  
+
+    return answer;
 }
 
 function runTest(i) {
