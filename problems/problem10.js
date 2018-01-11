383f84d0e8d059ca314790ceebd7c6d5695f20bd
Archer143 <Alex.F.April@hotmail.com>
2018-01-10 19:47:21 -0500

    Problem10.js is solved

--- problems/problem10.js
+++ problems/problem10.js
@@ -2,11 +2,19 @@ var assert = require('assert');
 
 // we need 5 test cases. 
 let inputs = [
-  
+    "Hello world",
+    "Bonjour le monde",
+    "ola sénorita",
+    "hELLO eVERYONE iN tHE wORLD",
+    "Koalas are cool"
 ]
 
 let outputs = [
-  
+    "Hello World",
+    "Bonjour Le Monde",
+    "Ola Sénorita",
+    "Hello Everyone In The World",
+    "Koalas Are Cool"
 ]
 
 /*
@@ -17,11 +25,23 @@ f("ALL YOUR BASE ARE BELONG"); // All Your Base Are Belong
 
 */
 function f(str) {
-    
+    var arr = str.toLowerCase().split(" ");
+    var firstChar = "";
+    var capWord = "";
+    var newArr = [];
+
+    for (var i = 0; i < arr.length; ++i) {
+        firstChar = arr[i].toUpperCase().charAt(0);
+        capWord = firstChar + arr[i].substring(1, arr[i].length);
+        newArr.push(capWord);
+    }
+
+    var newStr = newArr.join(" ");
+    return newStr;
 }
 
 function runTest(i) {
-    if(i > inputs.length) throw new Error("You do not have enough test cases");
+    if (i > inputs.length) throw new Error("You do not have enough test cases");
     var expected = outputs[i];
     var actual = f(inputs[i]);
     assert.deepEqual(actual, expected);
--- problems/problem9.js
+++ problems/problem9.js
@@ -2,19 +2,19 @@ var assert = require('assert');
 
 // we need 5 test cases. 
 let inputs = [
-  "hi",
-  "",
-  "Hello my anticonstitutionnalisationnable people",
-  "One two three",
-  "Racecar up side down is racecar"
+    "hi",
+    "",
+    "Hello my anticonstitutionnalisationnable people",
+    "One two three",
+    "Racecar up side down is racecar"
 ]
 
 let outputs = [
-  "hi",
-  "",
-  "anticonstitutionnalisationnable",
-  "three",
-  "racecar"
+    "hi",
+    "",
+    "anticonstitutionnalisationnable",
+    "three",
+    "racecar"
 ]
 
 /*
@@ -24,14 +24,14 @@ If multiple words have the same length, return the last one that matches.
 function f(str) {
     str.split(" ").toLowerCase();
     var lgStr = "";
-    for(var i = 0; i < str.length; ++i){
-        if(lgStr.length <= str[i].length) lgStr = str[i];
+    for (var i = 0; i < str.length; ++i) {
+        if (lgStr.length <= str[i].length) lgStr = str[i];
     }
     return lgStr;
 }
 
 function runTest(i) {
-    if(i > inputs.length) throw new Error("You do not have enough test cases");
+    if (i > inputs.length) throw new Error("You do not have enough test cases");
     var expected = outputs[i];
     var actual = f(inputs[i]);
     assert.deepEqual(actual, expected);
