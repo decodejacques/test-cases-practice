ab0d1532d107238145940443c93f3c766b7e1da1
Kamylle <kamylle.b.g@gmail.com>
2018-01-11 17:53:58 -0500

    Problem 12 Done

--- problems/problem12.js
+++ problems/problem12.js
@@ -2,11 +2,19 @@ var assert = require('assert');
 
 // we need 5 test cases. 
 let inputs = [
-  
+    [[0,1,2,3], [1,3,4,5]],
+    [[1,2,3], [1,2,3]],
+    [[1,2,42], [1,2,13]],
+    [[1], [1]],
+    ["a"]
 ]
 
 let outputs = [
-  
+    [0,2,4,5],
+    [],
+    [42,13],
+    [],
+    undefined
 ]
 
 /*
@@ -19,8 +27,40 @@ uniqueElements([0,1,2,3], [1,3,4,5]); // [0,4,5]
 uniqueElements([1,2,3], [1,2,3]); // []
 uniqueElements(2,3); // undefined, not arrays
 */
-function f(arr1, arr2) {
+
+function isPartOfArray(x, list){
+    for (var i = 0; i < list.length; i++) {
+        if (x == list[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function f(arr) {
+    var firstArray = arr[0];
+    var secondArray = arr[1];
+    var answer = [];
+
+    if (Array.isArray(arr) && Array.isArray(firstArray) && Array.isArray(secondArray)) {
+        for (var i = 0; i < firstArray.length; i++) {
+            if (!isPartOfArray(firstArray[i], secondArray) 
+            && !isPartOfArray(firstArray[i], answer)) {
+                answer.push(firstArray[i]);
+            }
+        }
+
+        for (var i = 0; i < secondArray.length; i++) {
+            if (!isPartOfArray(secondArray[i], firstArray) 
+            && !isPartOfArray(secondArray[i], answer)) {
+                answer.push(secondArray[i]);
+            }
+        }
+
+        return answer;
+    }
     
+
 }
 
 function runTest(i) {
