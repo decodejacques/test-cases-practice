d9fb596743f0a3f26016811bc575cf15b62cd3f1
Kamylle <kamylle.b.g@gmail.com>
2018-01-10 20:13:11 -0500

    Problem 7 Done

--- problems/problem7.js
+++ problems/problem7.js
@@ -2,11 +2,23 @@ var assert = require('assert');
 
 // we need 7 test cases. 
 let inputs = [
-  
+  ["to", 2],
+  ["bon", 2],
+  ["tic ", 4],
+  ["Zero", 0],
+  ["Negative", -3],
+  ["", 6],
+  ["Hello", "World"]
 ]
 
 let outputs = [
-  
+  "toto",
+  "bonbon",
+  "tic tic tic tic ",
+  "",
+  "",
+  "",
+  undefined
 ]
 
 /*
@@ -18,8 +30,20 @@ f(["fo", 3]) // "fofofo"
 f(["foo", -1]) // undefined
 */
 function f(arr) {
-    
+    var str = arr[0];
+    var num = arr[1];
+    if (typeof str == 'string' && typeof num == 'number') {
+        if (str.length > 0 && num > 0) {
+            return str.repeat(num);
+            console.log("--------" + arr + "Worked");
+        }
+        else {
+            return "";
+        }
+    }
 }
+
+f(["foo", 3])
 
 function runTest(i) {
     if(i > inputs.length) throw new Error("You do not have enough test cases");
